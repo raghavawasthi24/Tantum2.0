454c3fd7e48ee6d047203c2f1d1198df1fbef80a
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-05-09 06:14:07 +0530

    project initialized and connected database

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express, { Application, Request, Response } from "express";
+const PORT = process.env.PORT || 5000;
+const app: Application = express();
+import cors from "cors";
+import "./config/db";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use("/api", require("./routes/index.js"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "Hello, Welcome To This Page",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is live at PORT: ${PORT}`);
+});

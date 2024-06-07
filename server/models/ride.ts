e9fa614d28cabdc21d0688e207a3da46acb30499
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-06-07 15:30:06 +0530

    feat: schema and type of ride

---
+++ server/models/ride.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { RideSchema } from "../types/ride";
+
+const rideSchema = new mongoose.Schema<RideSchema>(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true,
+    },
+    destination: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    seatsVacant: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RideModel = mongoose.model<RideSchema>("User", rideSchema);
+
+export default RideModel;
---
+++ server/types/ride.ts
@@ -0,0 +1,11 @@
+import { Document, Types } from "mongoose";
+
+export interface RideSchema extends Document {
+  ownerId: Types.ObjectId;
+  vehicleType: string;
+  source: string;
+  destination: string;
+  date: Date;
+  time: string;
+  seatsVacant: number;
+}
